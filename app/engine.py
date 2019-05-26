10e0d2dcbe53e3190c729760cb7212ca7361eb47
bdintu <j@bdintu.com>
2019-05-26 21:43:52 +0700

    Use jsonfy

--- app/engine.py
+++ app/engine.py
@@ -1,10 +1,10 @@
+from flask import jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
-from json import dumps
 
 
 def engine():
     return create_engine('sqlite:///clinic.db').connect()
 
 def dump(result):
-    return dumps([dict(r) for r in result])
+    return jsonify([dict(row) for row in result])
